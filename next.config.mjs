b0fdf12b678be50c31e4649f8ec179737da4ed84
Venkat Pavan <pasamvenkatpavan@gmail.com>
2026-07-17 03:15:40 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'",
           },
           { key: "X-Frame-Options", value: "DENY" },
           { key: "X-Content-Type-Options", value: "nosniff" },
