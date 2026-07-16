6db1ec59a29b638b73bf9ebfe4419bfa57566f8b
Venkat Pavan <pasamvenkatpavan@gmail.com>
2026-07-17 01:48:36 +0530

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'",
+          },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
